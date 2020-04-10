1baf266af6a7e8ddb2f5dfdf36d214acb941f067
Greg Larsen <gslarsen2718@gmail.com>
2020-04-10 10:25:27 -0400

    improve modal rendering frequency using lifecycle method

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,39 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 
-import Button from '../../UI/Button/Button';
+import Button from "../../UI/Button/Button";
 
-const OrderSummary = (props) => {
-  const ingredients = Object.entries(props.ingredients).filter(
-    (ingredient) => ingredient[1]
-  );
+class OrderSummary extends Component {
 
-  return (
-    <Fragment>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul style={{ listStyleType: "none" }}>
-        {ingredients.map((ingredient) => (
-          <li key={ingredient[0]}>
-            <span style={{ textTransform: "capitalize" }}>{ingredient[0]}</span>
-            : {ingredient[1]}
-          </li>
-        ))}
-      </ul>
-      <h3>Total: &nbsp; ${props.totalPrice.toFixed(2)}</h3>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Fragment>
-  );
-};
+  render = () => {
+    const ingredients = Object.entries(this.props.ingredients).filter(
+      (ingredient) => ingredient[1]
+    );
+
+    return (
+      <Fragment>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul style={{ listStyleType: "none" }}>
+          {ingredients.map((ingredient) => (
+            <li key={ingredient[0]}>
+              <span style={{ textTransform: "capitalize" }}>
+                {ingredient[0]}
+              </span>
+              : {ingredient[1]}
+            </li>
+          ))}
+        </ul>
+        <h3>Total: &nbsp; ${this.props.totalPrice.toFixed(2)}</h3>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCanceled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Fragment>
+    );
+  };
+}
 
 export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,30 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 
 import classes from "./Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 
-const Modal = (props) => (
-  <Fragment>
-    <Backdrop show={props.show} modalClosed={props.modalClosed}/>
-    <div
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0",
-      }}
-      className={classes.Modal}
-    >
-      {props.children}
-    </div>
-  </Fragment>
-);
+class Modal extends Component {
+
+  shouldComponentUpdate = (nextProps, nextState) => {
+    return nextProps.show !== this.props.show;
+  }
+
+  render = () => {
+    return (
+      <Fragment>
+        <Backdrop show={this.props.show} modalClosed={this.props.modalClosed} />
+        <div
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0",
+          }}
+          className={classes.Modal}
+        >
+          {this.props.children}
+        </div>
+      </Fragment>
+    );
+  }
+}
 
 export default Modal;
